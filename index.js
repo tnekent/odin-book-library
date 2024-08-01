3ee6e7cc10ce570f8c06cb46f9e97603edd6d90d
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-08-01 18:24:36 +0800

    Track added books in a global array

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+const myLibrary = [];
+
 function Book(name, author, pageCount, hasRead) {
     this.name = name;
     this.author = author;
     this.pageCount = pageCount;
     this.hasRead = hasRead;
+}
+
+function addBookToLibrary(name, author, pageCount, hasRead) {
+    const book = new Book(name, author, pageCount, hasRead);
+    myLibrary.push(book);
+}
+
+function test() {
+    addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, false);
+    addBookToLibrary("Lord of the Flies", "William Golding", 224, true);
+    console.table(myLibrary);
 }
