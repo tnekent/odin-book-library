7a5b29b093b560b0aa520079f98d6af553ad19cf
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-08-02 00:43:24 +0800

    Show all book entries to table

--- index.js
+++ index.js
@@ -12,8 +12,26 @@ function addBookToLibrary(name, author, pageCount, hasRead) {
     myLibrary.push(book);
 }
 
+function displayBookToTable(book, table) {
+    const bookRow = document.createElement("tr");
+    const bookProps = Object.keys(book);
+    for (let i = 0; i < bookProps.length; i++) {
+        const bookCell = document.createElement("td");
+        bookCell.textContent = book[bookProps[i]];
+        bookRow.appendChild(bookCell);
+    }
+    table.appendChild(bookRow);
+}
+
+function displayLibraryToTable() {
+    const bookTableBody = document.querySelector("#book-table > tbody");
+    myLibrary.forEach(book => displayBookToTable(book, bookTableBody));
+}
+
 function test() {
     addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, false);
     addBookToLibrary("Lord of the Flies", "William Golding", 224, true);
-    console.table(myLibrary);
+    displayLibraryToTable();
 }
+
+test();
