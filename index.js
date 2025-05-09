0995f370f23d0e10f56837a181dc79bc6bf20f3d
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2025-05-09 19:24:18 +0800

    Refactor Book to a class

--- index.js
+++ index.js
@@ -1,107 +1,109 @@
 const myLibrary = [];
 const bookTableBody = document.querySelector("#book-table > tbody");
 
-function Book(name, author, pageCount, hasRead) {
+class Book {
+  constructor(name, author, pageCount, hasRead) {
     this.name = name;
     this.author = author;
     this.pageCount = pageCount;
     this.hasRead = hasRead ? "Have read" : "To read";
-}
+  }
 
-Book.prototype.toggleReadStatus = function() {
+  toggleReadStatus() {
     if (this.hasRead === "Have read") {
-        this.hasRead = "To read";
+      this.hasRead = "To read";
     } else {
-        this.hasRead = "Have read";
+      this.hasRead = "Have read";
     }
+  }
 }
 
 function addBookToLibrary(name, author, pageCount, hasRead) {
-    const book = new Book(name, author, pageCount, hasRead);
-    myLibrary.push(book);
+  const book = new Book(name, author, pageCount, hasRead);
+  myLibrary.push(book);
 }
 
 function removeBook(e) {
-    const bookRow = e.currentTarget.parentNode.parentNode;
-    const bookRowIndex = Array.prototype.indexOf.call(bookTableBody.children, bookRow);
-    myLibrary.splice(bookRowIndex, 1);
-    bookTableBody.removeChild(bookRow);
+  const bookRow = e.currentTarget.parentNode.parentNode;
+  const bookRowIndex = Array.prototype.indexOf.call(bookTableBody.children, bookRow);
+  myLibrary.splice(bookRowIndex, 1);
+  bookTableBody.removeChild(bookRow);
 }
 
 function createRemoveCell() {
-    const rmvCell = document.createElement("td");
-    const rmvBtn = document.createElement("button");
-    const rmvIcon = document.createElement("span");
-    rmvIcon.classList.add("material-symbols-outlined");
-    rmvIcon.textContent = "delete";
-    rmvBtn.appendChild(rmvIcon);
-    rmvCell.appendChild(rmvBtn);
-    rmvCell.classList.add("rmv-cell");
-    
-    rmvBtn.addEventListener("click", removeBook);
-    
-    return rmvCell;
+  const rmvCell = document.createElement("td");
+  const rmvBtn = document.createElement("button");
+  const rmvIcon = document.createElement("span");
+  rmvIcon.classList.add("material-symbols-outlined");
+  rmvIcon.textContent = "delete";
+  rmvBtn.appendChild(rmvIcon);
+  rmvCell.appendChild(rmvBtn);
+  rmvCell.classList.add("rmv-cell");
+
+  rmvBtn.addEventListener("click", removeBook);
+
+  return rmvCell;
 }
 
 function setToggleIndicator(toggleBtn, readState) {
-    const toggleIcon = toggleBtn.children[0];
-    if (readState === "Have read") {
-        toggleIcon.textContent = "cancel";
-        toggleBtn.className = "to-false";
-    } else {
-        toggleIcon.textContent = "check_circle";
-        toggleBtn.className = "to-true";
-    }
+  const toggleIcon = toggleBtn.children[0];
+  if (readState === "Have read") {
+    toggleIcon.textContent = "cancel";
+    toggleBtn.className = "to-false";
+  } else {
+    toggleIcon.textContent = "check_circle";
+    toggleBtn.className = "to-true";
+  }
 }
 
 function toggleReadStatus(e) {
-    const bookRow = e.currentTarget.parentNode.parentNode;
-    const bookRowIndex = Array.prototype.indexOf.call(bookTableBody.children, bookRow);
-    const book = myLibrary[bookRowIndex];
-    book.toggleReadStatus();
-    setToggleIndicator(e.currentTarget, book.hasRead);
-    bookRow.children[3].textContent = book.hasRead;
+  const bookRow = e.currentTarget.parentNode.parentNode;
+  const bookRowIndex = Array.prototype.indexOf.call(bookTableBody.children, bookRow);
+  const book = myLibrary[bookRowIndex];
+  book.toggleReadStatus();
+  setToggleIndicator(e.currentTarget, book.hasRead);
+  bookRow.children[3].textContent = book.hasRead;
 }
 
 function createToggleReadCell(initReadState) {
-    const toggleCell = document.createElement("td");
-    const toggleBtn = document.createElement("button");
-    const tglIcon = document.createElement("span");
-    tglIcon.classList.add("material-symbols-outlined");
-    tglIcon.textContent = "check_circle";
-    toggleBtn.appendChild(tglIcon);
-    setToggleIndicator(toggleBtn, initReadState);
-    toggleCell.appendChild(toggleBtn);
-    toggleCell.classList.add("tgl-cell");
+  const toggleCell = document.createElement("td");
+  const toggleBtn = document.createElement("button");
+  const tglIcon = document.createElement("span");
+  tglIcon.classList.add("material-symbols-outlined");
+  tglIcon.textContent = "check_circle";
+  toggleBtn.appendChild(tglIcon);
+  setToggleIndicator(toggleBtn, initReadState);
+  toggleCell.appendChild(toggleBtn);
+  toggleCell.classList.add("tgl-cell");
 
-    toggleBtn.addEventListener("click", toggleReadStatus);
+  toggleBtn.addEventListener("click", toggleReadStatus);
 
-    return toggleCell;
+  return toggleCell;
 }
 
 
 function displayBookToTable(book) {
-    const bookRow = document.createElement("tr");
-    const bookProps = Object.keys(book);
-    for (let i = 0; i < bookProps.length; i++) {
-        const bookCell = document.createElement("td");
-        bookCell.textContent = book[bookProps[i]];
-        bookRow.appendChild(bookCell);
-    }
-    bookRow.appendChild(createToggleReadCell(book.hasRead));
-    bookRow.appendChild(createRemoveCell());
-    bookTableBody.appendChild(bookRow);
+  const bookRow = document.createElement("tr");
+  const bookProps = Object.keys(book);
+  for (let i = 0; i < bookProps.length; i++) {
+    const bookCell = document.createElement("td");
+    bookCell.textContent = book[bookProps[i]];
+    bookRow.appendChild(bookCell);
+  }
+  bookRow.appendChild(createToggleReadCell(book.hasRead));
+  bookRow.appendChild(createRemoveCell());
+  bookTableBody.appendChild(bookRow);
 }
 
 function displayLibraryToTable() {
-    const bookTableBody = document.querySelector("#book-table > tbody");
-    myLibrary.forEach(displayBookToTable);
+  const bookTableBody = document.querySelector("#book-table > tbody");
+  myLibrary.forEach(displayBookToTable);
 }
 
 const addBookBtn = document.querySelector("#add-book");
 const addBookDlg = document.querySelector("dialog");
 addBookBtn.addEventListener("click", e => {
-    addBookDlg.showModal();
+  addBookDlg.showModal();
 })
 
 const confirmBtn = addBookDlg.querySelector("#confirm-btn");
@@ -109,29 +111,29 @@ const addBookInputs = addBookDlg.querySelectorAll("input");
 const dialogForm = addBookDlg.querySelector("form");
 
 confirmBtn.addEventListener("click", () => {
-    const inputValues = [];
+  const inputValues = [];
 
-    addBookInputs.forEach(input => {
-        const inputValueProp =
-            input.type === "checkbox" ? 
-            "checked" : "value";
-        let inputValue = input[inputValueProp];
+  addBookInputs.forEach(input => {
+    const inputValueProp =
+      input.type === "checkbox" ?
+        "checked" : "value";
+    let inputValue = input[inputValueProp];
 
-        inputValues.push(inputValue);
-    });
+    inputValues.push(inputValue);
+  });
 
-    addBookToLibrary.apply(null, inputValues);
-    displayBookToTable(myLibrary[myLibrary.length - 1]);
-    dialogForm.reset();
+  addBookToLibrary.apply(null, inputValues);
+  displayBookToTable(myLibrary[myLibrary.length - 1]);
+  dialogForm.reset();
 });
 
 const cancelBtn = addBookDlg.querySelector("#cancel-btn");
 cancelBtn.addEventListener("click", dialogForm.reset.bind(dialogForm));
 
 function test() {
-    addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, false);
-    addBookToLibrary("Lord of the Flies", "William Golding", 224, true);
-    displayLibraryToTable();
+  addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, false);
+  addBookToLibrary("Lord of the Flies", "William Golding", 224, true);
+  displayLibraryToTable();
 }
 
 test();
